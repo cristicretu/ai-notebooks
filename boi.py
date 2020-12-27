dc7b2c03b3b521c6b6b4598a17be6199d2e609fb
cristicretu <cristicretu38@gmail.com>
2020-12-27 14:48:32 +0200

    boi

---
+++ boi.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+fashion_mnist = tf.keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images,
+                               test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+print(train_images.shape)
